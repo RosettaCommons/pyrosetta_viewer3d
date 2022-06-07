1a3cdf55add49e42a077f74c201a688413985c04
Ajasja Ljubetič <ajasja.ljubetic@gmail.com>
2022-06-07 16:19:08 +0200

    Typos in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup,
 
 setup(
     name="viewer3d",
@@ -7,14 +7,14 @@ setup(
     url="https://github.com/RosettaCommons/pyrosetta_viewer3d",
     author="Jason C. Klima, Ajasja Ljubetic",
     license="MIT",
-    packages=['viewer3d'] #these are the files that get packaged up
+    packages=['viewer3d'], # these are the files that get packaged up
     install_requires=[
         "attrs>=18.2.0",
         "ipywidgets>=7.5.1",
         "jupyter>=1.0.0",
         "py3Dmol>=1.8.0",
-        # TODO: add conditinal dependancies, for example pip install pyrosetta_viewer3d[nglview]
-        # "pyrosetta>=2022.21", #Pyrosetta is not on pip, so adding it as an explicit dependancy would only casue problems. 
+        # TODO: add conditional dependencies, for example pip install pyrosetta_viewer3d[nglview]
+        # "pyrosetta>=2022.21", # Pyrosetta is not on pip, so adding it as an explicit dependency would only cause problems.  
     ],
     zip_safe=False,
 )
