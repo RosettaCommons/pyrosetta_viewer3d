d04bca34dc2a7f1af1524df1c908a6a9786d6588
Ajasja Ljubetič <ajasja.ljubetic@gmail.com>
2022-06-07 16:15:36 +0200

    Fix dependencies in setup.py
    `install_requires` is the correct way to specify dependencies, packages is used to package up directories.

--- setup.py
+++ setup.py
@@ -5,14 +5,16 @@ setup(
     version="1.0.0",
     description="Display PackedPose objects, Pose objects, or PDB files within a Jupyter notebook and Google Colab.",
     url="https://github.com/RosettaCommons/pyrosetta_viewer3d",
-    author="Jason C. Klima",
+    author="Jason C. Klima, Ajasja Ljubetic",
     license="MIT",
-    packages=[
+    packages=['viewer3d'] #these are the files that get packaged up
+    install_requires=[
         "attrs>=18.2.0",
         "ipywidgets>=7.5.1",
         "jupyter>=1.0.0",
         "py3Dmol>=1.8.0",
-        "pyrosetta>=2022.21",
+        # TODO: add conditinal dependancies, for example pip install pyrosetta_viewer3d[nglview]
+        # "pyrosetta>=2022.21", #Pyrosetta is not on pip, so adding it as an explicit dependancy would only casue problems. 
     ],
     zip_safe=False,
 )
