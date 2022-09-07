f5f92f0d061e642a4b8392ab17fe6f53c9408c8c
Ajasja Ljubetič <ajasja.ljubetic@gmail.com>
2022-09-07 14:21:14 +0200

    Fix matplotlib dependency
    On gooogle colab, which is running python 3.7.13 by default, matplotlib 3.5 works best as dependency. 3.5.2 raises an erorr.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "bokeh>=2.4.2",
         "ipywidgets>=7.5.1",
         "jupyter>=1.0.0",
-        "matplotlib>=3.5.2",
+        "matplotlib>=3.5",
         "py3Dmol>=1.8.0",
         # TODO: add conditional dependencies, for example pip install pyrosetta_viewer3d[nglview]
         # "pyrosetta>=2022.21", # Pyrosetta is not on pip, so adding it as an explicit dependency would only cause problems.
